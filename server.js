247b301797bda247f1dffb83cef2babe17b04264
Ziyak Jehangir <ziyak97@gmail.com>
2020-06-18 04:42:27 +0530

    Fixed join bug.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 // If production serve static files
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname + 'client/build')))
+    app.use(express.static(path.join(__dirname, 'client/build')))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
